6d372f40f3693daa5e2eb2a49729242dcdb559a6
Yunfan Hu <1033718596@qq.com>
2020-12-28 16:31:09 +0800

    run code

---
+++ run.sh
@@ -0,0 +1,6 @@
+python preprocess.py --dataset=diginetica
+python preprocess.py --dataset=yoochoose
+
+python build_global_graph.py --dataset=diginetica
+python build_global_graph.py --dataset=yoochoose1_64
+python build_global_graph.py --dataset=yoochoose1_4
